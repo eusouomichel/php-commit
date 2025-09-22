f220b350d480a18c4f2e537c02414e3d4e4e9d05
Michel Miléski <michelmileski@Mac-mini-de-Michel.local>
2025-09-22 17:30:54 -0300

    chore: bump version to 1.0.2

--- src/Application.php
+++ src/Application.php
@@ -10,7 +10,7 @@ class Application extends BaseApplication
 {
     public function __construct()
     {
-        parent::__construct('PHP Commit', '1.0.1');
+        parent::__construct('PHP Commit', '1.0.2');
 
         // Registra os comandos
         $this->add(new InitCommand());
