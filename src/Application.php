32762119e7b4a3ef2324a80709b8c396197fb8c2
Michel Miléski <michelmileski@Mac-mini-de-Michel.local>
2025-09-22 18:01:03 -0300

    chore: update version to 1.0.4 with complete Portuguese translations

--- src/Application.php
+++ src/Application.php
@@ -10,7 +10,7 @@ class Application extends BaseApplication
 {
     public function __construct()
     {
-        parent::__construct('PHP Commit', '1.0.3');
+        parent::__construct('PHP Commit', '1.0.4');
 
         // Registra os comandos
         $this->add(new InitCommand());
